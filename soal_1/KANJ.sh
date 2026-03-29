600022205e425d3f71d1bb291d32b4108f1cafe7
Maitasya <aksabrata251@gmail.com>
2026-03-29 15:14:01 +0700

    pesan perubahan

--- soal_1/KANJ.sh
+++ soal_1/KANJ.sh
@@ -62,7 +62,7 @@ END {
         print oldest " adalah penumpang kereta tertua dengan usia " max_age " tahun"
     }
     else if (soal == "d") {
-        average_age = int(total_usia / total_passenger + 0.5) #dibulatin aja ke atas
+        average_age = int(total_usia / total_passenger ) #dibulatin aja ke atas
         print "Rata-rata usia penumpang adalah " average_age " tahun"
     }
     else if (soal == "e") {
